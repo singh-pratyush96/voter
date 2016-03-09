9f18c06386b0089ee2b17e993babc3309a5d747a
Pratyush Singh <singh.pratyush96@gmail.com>
2016-03-10 02:35:35 +0530

    Improved http404 raising for non existing objects.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -14,10 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        q = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question doesn\'t exists')
+    q = get_object_or_404(Question, pk=question_id)
+
     context = {
         'question': q
     }
