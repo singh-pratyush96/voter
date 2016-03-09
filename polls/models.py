7fd99b9d4700b6db0049df61c8c64e13c6ad4e78
Pratyush Singh <singh.pratyush96@gmail.com>
2016-03-09 20:50:30 +0530

    Added latency check for Question

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +11,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
