2164ec071b8fb28ad393811bcb9372029d0672d0
Pratyush Singh <singh.pratyush96@gmail.com>
2016-03-10 13:56:20 +0530

    Namespaced views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Choice
@@ -24,9 +24,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
